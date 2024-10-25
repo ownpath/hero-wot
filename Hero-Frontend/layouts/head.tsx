9bb9b4d7713a50563995dcd33e34584276f9c101
Suprateek <suprateek24@gmail.com>
2024-10-25 17:53:42 +0530

    Fixed meta

--- Hero-Frontend/layouts/head.tsx
+++ Hero-Frontend/layouts/head.tsx
@@ -9,22 +9,45 @@ export const Head = () => {
 
   return (
     <NextHead>
-      <title>{siteConfig.name}</title>
-      <meta key="title" content={siteConfig.name} property="og:title" />
-      <meta content={siteConfig.description} property="og:description" />
-      <meta content={siteConfig.description} name="description" />
+      <title>Share your heartfelt wishes for #OurHeroAt70</title>
       <meta
-        key="viewport"
-        content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        name="viewport"
+        name="description"
+        content="As we approach our chairman’s 70th birthday, we invite you to share your love, cherished memories, and heartfelt wishes.
+"
       />
 
-      <meta property="og:image" content={`${baseUrl}/metapic.png`} />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
+      <meta property="og:url" content="https://www.pawanmunjal.life/" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={baseUrl} />
+      <meta
+        property="og:title"
+        content="Share your heartfelt wishes for #OurHeroAt70"
+      />
+      <meta
+        property="og:description"
+        content="As we approach our chairman’s 70th birthday, we invite you to share your love, cherished memories, and heartfelt wishes.
+"
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/images/f93181af-e52a-418a-98a8-c465a447bd3e.png?token=Qj39UaBJ9FBdAFeYhbIWL4PLU_oCIWOf5IScUDLxJ6k&height=672&width=1200&expires=33265858769"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="pawanmunjal.life" />
+      <meta property="twitter:url" content="https://www.pawanmunjal.life/" />
+      <meta
+        name="twitter:title"
+        content="Share your heartfelt wishes for #OurHeroAt70"
+      />
+      <meta
+        name="twitter:description"
+        content="As we approach our chairman’s 70th birthday, we invite you to share your love, cherished memories, and heartfelt wishes.
+"
+      />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/images/f93181af-e52a-418a-98a8-c465a447bd3e.png?token=Qj39UaBJ9FBdAFeYhbIWL4PLU_oCIWOf5IScUDLxJ6k&height=672&width=1200&expires=33265858769"
+      ></meta>
 
       <link href="/favicon.ico" rel="icon" />
     </NextHead>
