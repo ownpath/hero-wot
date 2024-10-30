efc0cdeb4a88dffcf7f9ba73a3d883ab3bf5d84a
Suprateek <suprateek24@gmail.com>
2024-10-30 13:02:22 +0530

    prevent right click

--- Hero-Frontend/pages/wallofwishes/index.tsx
+++ Hero-Frontend/pages/wallofwishes/index.tsx
@@ -1,14 +1,21 @@
 import AdminGuard from "@/components/AdminGuard";
 import PostsMasonryLayout from "@/components/MasonryGrid";
-
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import { Navbar } from "../../components/NavBar";
 import { Toaster } from "sonner";
 
 export default function Masonry() {
+  // Function to disable right-click context menu
+  const disableRightClick = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="min-h-screen">
+    <div
+      className="min-h-screen select-none" // Prevent text selection with Tailwind
+      onContextMenu={disableRightClick} // Disable right-click
+    >
       <Navbar />
       <div className="pt-32">
         <PostsMasonryLayout />
